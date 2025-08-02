5a8cbe810a18762d5d296e1fbdc8af7e0702270f
Chaiyapruek Muangsiri <cmp.poon@gmail.com>
2025-08-02 23:12:31 +0800

    fix catalog typo

--- src/data/catalog.ts
+++ src/data/catalog.ts
@@ -1,13 +1,13 @@
 export const catalogs = [
   {
     id: 1,
-    name: "แคตาล็อกสินค้าทั้งหมด",
+    name: "แคตตาล็อกสินค้าทั้งหมด",
     image: "/catalogs/catalog1.webp",
     url: "/catalogs/KSCATALOG_2025_compressed.pdf",
   },
   {
     id: 2,
-    name: "แคตาล็อกแผ่นปิดรอยต่อ",
+    name: "แคตตาล็อกแผ่นปิดรอยต่อ",
     image: "/catalogs/catalog2.webp",
     url: "/catalogs/แผ่นปิดรอยต่อ_สีดำ_KS.pdf",
   },
