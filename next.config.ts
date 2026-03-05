ecf08d4cc07507aa209872cee6c7e3000775bfba
ssuraruangchai-cyber <ssuraruangchai@gmail.com>
2026-03-05 20:33:03 +0700

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [{ type: "host", value: "kaistandard.com" }],
+        destination: "https://www.kaistandard.com/:path*",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
